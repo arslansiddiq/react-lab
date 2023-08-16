946656bf02297931a2905e8cb1c5f1647c0dd4a4
Arslan Siddique <arsi.555@gmail.com>
2023-08-16 18:40:15 +0500

    Model Button design Change.

--- src/generate-embeddable-html.tsx
+++ src/generate-embeddable-html.tsx
@@ -170,7 +170,7 @@ export const generateEmbeddableHTML = (labDistPath = "lab/") => `
 
     <div id="sensorModal">
       <div id="modalButtonContainer">
-        <button id="sensor-close">Close</button>
+        <button id="sensor-close"><?xml version='1.0' ?><svg width='16' height='16' viewBox='0 0 16 16' xmlns='http://www.w3.org/2000/svg'><polygon fill-rule='evenodd' points='8 9.414 3.707 13.707 2.293 12.293 6.586 8 2.293 3.707 3.707 2.293 8 6.586 12.293 2.293 13.707 3.707 9.414 8 13.707 12.293 12.293 13.707 8 9.414'/></svg></button>
       </div>
       <div id="sensorModal-content">
         <p>Drag and drop sensors on the model</p>
